df2c7a2578036e7422b17c5e99b7613f8a061876
Alex Schouls <alex.schouls@valtech.com>
2022-11-08 09:22:19 +0100

    Added SSR example of external API

---
+++ pages/Ssr.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>A server-side rendered page:</h2>
+        <p>{{ text[0] }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Loading...'
+        }
+    },
+    async fetch() {
+        this.text = await fetch('https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1').then(res => res.json());
+    }
+}
+</script>
