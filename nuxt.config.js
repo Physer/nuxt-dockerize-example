d65b528128268fac30630dca02ece6150f8d2cc9
Alex Schouls <alex.schouls@valtech.com>
2022-11-10 13:56:43 +0100

    Added a middleware module

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,9 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '~/modules/middlewareModule'
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
