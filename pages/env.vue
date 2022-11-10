ed24aabf18006a30deb965e79e07de89758ea23f
Alex Schouls <alex.schouls@valtech.com>
2022-11-10 13:46:02 +0100

    Merged the non-nuxt-bridge version with the nuxt-bridge in one branch

---
+++ pages/env.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>This page shows environment variables:</h2>
+        <p>[Public] Application name: {{ this.$config.applicationName }}</p>
+        <p>A private environment variable is only visible on the server (see console log) - should be printed here: {{ this.$config.secret }}</p>
+        <p>If you do not see a secret in your browser, that's expected behaviour</p>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('A secret environment variable is: ', this.$config.secret);
+    }
+}
+</script>
