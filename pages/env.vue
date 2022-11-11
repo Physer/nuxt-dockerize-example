140a4956ae1dbcff98114a06934c25469503a23a
Alex Schouls <alex.schouls@valtech.com>
2022-11-11 13:26:56 +0100

    Fixed error in env.vue

--- pages/env.vue
+++ pages/env.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2>This page shows environment variables:</h2>
-        <p>[Public] Application name: {{ this.$config.public.applicationName }}</p>
+        <p>[Public] Application name: {{ this.$config.applicationName }}</p>
         <p>A private environment variable is only visible on the server (see console log) - should be printed here: {{ this.$config.secret }}</p>
         <p>If you do not see a secret in your browser, that's expected behaviour</p>
     </div>
