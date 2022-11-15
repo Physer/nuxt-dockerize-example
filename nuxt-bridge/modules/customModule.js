2c031bdfabf585b86013df760217cf07780226d2
Alex Schouls <alex.schouls@valtech.com>
2022-11-15 13:20:04 +0100

    Removing obsolete code

--- nuxt-bridge/modules/customModule.js
+++
@@ -1,5 +0,0 @@
-export default function MiddlewareModule(_moduleOptions) {
-  this.nuxt.hook("ready", async (nuxt) => {
-    nuxt.options.cli.badgeMessages.push("Custom module: Initialized");
-  });
-}
--- nuxt-bridge/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h2>A statically generated page.</h2>
-    <p>This is static content</p>
-  </div>
-</template>
