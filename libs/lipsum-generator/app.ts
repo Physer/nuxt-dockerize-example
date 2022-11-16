f83d62f2aaeef6c8d0195970d9f0d1c940faff27
Alex Schouls <alex.schouls@valtech.com>
2022-11-16 16:24:24 +0100

    started on internal lib and workspaces

---
+++ libs/lipsum-generator/app.ts
@@ -0,0 +1,7 @@
+// import { LoremIpsum } from "lorem-ipsum";
+
+export default function generateLoremIpsum() {
+  // const lorem = new LoremIpsum();
+  // return lorem.generateSentences();
+  return "Hello there";
+}
---
+++ pages/lipsum.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>This page generates a piece of lorem ipsum through a private module:</h2>
+        <p>{{ lipsum }}</p>
+    </div>
+</template>
+
+<script>
+import generateLoremIpsum from '@physer/lipsum-generator'
+
+export default {
+    data() {
+        return {
+            lipsum: ''
+        }
+    },
+    fetch() {
+        this.lipsum = generateLoremIpsum();
+    }
+}
+</script>
