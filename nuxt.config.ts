9eca42e0cdadbbe74499b757fb66bbfdf12b7a76
Alex Schouls <alex.schouls@valtech.com>
2022-12-15 11:03:32 +0100

    Fixed /env page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      applicationName: process.env.APPLICATION_NAME,
+      applicationName: '',
     },
-    secret: process.env.SECRET,
+    secret: ''
   },
 });
