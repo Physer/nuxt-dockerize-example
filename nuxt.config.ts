7e72e4f48b60cc3bdade19a30b0065042a797d38
Alex Schouls <alex.schouls@valtech.com>
2022-12-15 10:27:34 +0100

    Updated nuxt.config.ts

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "example",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      applicationName: process.env.APPLICATION_NAME,
+    },
+    secret: process.env.SECRET,
+  },
+});
