7c34656ee0a32be3359f981217e0e43fff394ee7
Alex Schouls <alex.schouls@valtech.com>
2022-12-15 11:29:04 +0100

    Fixed the /ssr page

--- pages/ssr.vue
+++ pages/ssr.vue
@@ -1,20 +1,11 @@
+<script setup>
+console.log('This log line can show up on the server or the client, depending on how the page is rendered!');
+const text = await useFetch('https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1');
+</script>
+
 <template>
     <div>
         <h2>A server-side rendered page:</h2>
-        <p>{{ text[0] }}</p>
+        <p>{{ text.data }}</p>
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            text: ''
-        }
-    },
-    async fetch() {
-        console.log('This log line can show up on the server or the client, depending on how the page is rendered!');
-        this.text = await fetch('https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1').then(res => res.json());
-    }
-}
-</script>
